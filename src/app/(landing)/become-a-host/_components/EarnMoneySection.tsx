d0d6122b5d2a60f04587d348b5142010a010b158
ehiz <ehizomokhomion@gmail.com>
2025-03-31 11:32:25 +0100

    update earn money secton

--- src/app/(landing)/become-a-host/_components/EarnMoneySection.tsx
+++ src/app/(landing)/become-a-host/_components/EarnMoneySection.tsx
@@ -5,45 +5,44 @@ import Link from "next/link";
 
 export default function EarnMoneySection() {
   return (
-    <section className="relative overflow-hidden py-32">
+    <section className="relative overflow-hidden py-[120px]">
       <Shell>
-        <div className="flex flex-col lg:flex-row items-center justify-between">
+        <div className="flex flex-col lg:grid lg:grid-cols-2 items-center gap-[136px]">
           <div className="flex flex-col gap-6">
-            <h3 className="text-xl font-bold">
-              For Renters (Find & Book Vehicles)
+            <h3 className="heading-3 ">
+              Earn money while others drive your vehicle!
             </h3>
 
+            {/* earn money section */}
             <div className="space-y-5">
-              <p className="flex gap-3 items-start">
-                <TickCircleIcon />
-                <span>
-                  No long-term commitments—rent for a day, a week, or more
-                </span>
-              </p>
-              <p className="flex gap-3 items-start">
-                <TickCircleIcon />
-                <span>Choose from adventure, work, or specialty vehicles.</span>
-              </p>
-              <p className="flex gap-3 items-start">
-                <TickCircleIcon />
-                <span>Option for vehicle delivery at your convenience.</span>
-              </p>
+              {[
+                "Turn your vehicle into income when it's not in use.",
+                "You control pricing, availability, and rental rules.",
+                "Kaparki handles secure payments & bookings.",
+                "Stay protected – Renters are verified, and insurance is required.",
+              ].map((benefit, index) => (
+                <p
+                  key={index}
+                  className="flex gap-3 items-start">
+                  <TickCircleIcon />
+                  <span className="body-1-medium font-normal">{benefit}</span>
+                </p>
+              ))}
             </div>
 
             <div className="flex gap-4 mt-4">
-              <Button variant="cta" className="w-max">
-                <Link href="/" className="button-text">
-                  How it works
-                </Link>
-              </Button>
-              <Button variant="link" className="w-max">
-                <Link href="/" className="button-text">
-                  See reviews
+              <Button
+                variant="cta"
+                className="w-max">
+                <Link
+                  href="/"
+                  className="button-text">
+                  Start Listing Now
                 </Link>
               </Button>
             </div>
           </div>
-
+          {/* getting started */}
           <div className="space-y-8">
             <div className="relative">
               <div className="space-y-8">
@@ -53,27 +52,40 @@ export default function EarnMoneySection() {
                   </span>
                   Getting Started is Simple
                 </p>
-                {Array.from({ length: 3 }).map((_, index) => (
+                {[
+                  {
+                    title: "List Your Vehicle",
+                    description:
+                      "Upload high-quality photos, set your rental price, and choose your availability.",
+                  },
+                  {
+                    title: "Get Verified & Receive Bookings",
+                    description:
+                      "Once listed, renters will find your vehicle and request to book, or book directly if applicable.",
+                  },
+                  {
+                    title: "Earn Hassle-Free",
+                    description:
+                      "Kaparki ensures secure payments, verified renters, and seamless bookings..",
+                  },
+                ].map((step, index) => (
                   <div
                     key={index}
                     className={
-                      "relative bg-white w-[500px] min-h-[154px] rounded-3xl border p-4 gap-2 flex justify-between" +
+                      "relative bg-white w-[500px] min-h-[125px] rounded-3xl border p-4 gap-2 flex justify-between" +
                       (index < 2
                         ? " after:content after:inline-block after:h-8 after:w-1 after:bg-[#DBA806] after:absolute after:left-10 after:-z-10 after:-bottom-8 after:transform after:-translate-x-1/2"
                         : "")
-                    }
-                  >
+                    }>
                     <div>
                       <div className="border border-black flex items-center justify-center w-8 h-8 body-1-black bg-[#FFFEEE] rounded-full">
                         {index + 1}
                       </div>
                     </div>
                     <div className="space-y-2">
-                      <h3 className="body-1-black ">Find the Right Vehicle</h3>
+                      <h3 className="body-1-black ">{step.title}</h3>
                       <p className="body-1-medium text-black/80">
-                        Enter your location and dates to browse a variety of
-                        vehicles—from everyday cars to camper vans, motorcycles,
-                        and specialty rides.
+                        {step.description}
                       </p>
                     </div>
                   </div>
