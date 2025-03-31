e94c13f135924c1b30d4287f4351e4be99e04c69
ehiz <ehizomokhomion@gmail.com>
2025-03-31 11:44:26 +0100

    become a host

--- src/app/(landing)/become-a-host/_components/BuiltForHosts.tsx
+++ src/app/(landing)/become-a-host/_components/BuiltForHosts.tsx
@@ -7,15 +7,17 @@ export default function BuiltForHosts() {
     <section className="py-[120px]">
       <Shell>
         <p className="heading-3 text-center pb-16">Built for hosts like you</p>
-        <div className="grid grid-cols-2 gap-y-16 gap-x-32">
+        <div className="grid grid-cols-2 gap-y-16 gap-x-[120px]">
           {BUILT_FOR_HOSTS_DATA.map((item) => (
-            <div key={item.title}>
+            <div
+              key={item.title}
+              className="max-w-[469px]">
               <p className="heading-4 pb-4">{item.title}</p>
               <p className="body-1-medium text-black/80">{item.description}</p>
             </div>
           ))}
         </div>
-        <div className="flex justify-center pt-16 pb-6">
+        <div className="flex justify-center pt-16 pb-8">
           <Button variant="cta">Start earning on Kaparki</Button>
         </div>
       </Shell>
